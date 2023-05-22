5cb9ac4e98eef6ccf3c7fe95da7f183f92d29f7a
IrisWRX <wxu64@my.bcit.ca>
2023-05-22 13:01:15 -0700

    added reset, start, and difficulty level buttons

--- index.js
+++ index.js
@@ -4,137 +4,174 @@ const setup = async () => {
   let openCards = 0;
   let matchedCards = 0;
   let clickCount = 0;
-  const numpairs = 3;
   const gameGrid = $("#game-grid");
   const timeLimit = 100;
   let elapsedTime = 0;
   let timer;
+  let difficulty = { pairs: 3, duration: 100 };
 
-  // Check if all pairs are matched
-  function checkWin() {
-    if (matchedCards === numpairs) {
-      alert("You win!");
-      clearInterval(timer);
-    }
-  }
-
-  // Start timer
-  function startTimer() {
-    $("#timer").text(`You got ${timeLimit} seconds. 0 seconds passed.`);
-    timer = setInterval(() => {
-      elapsedTime++;
-      $("#timer").text(
-        `You got ${timeLimit} seconds. ${elapsedTime} seconds passed.`
-      );
-      if (elapsedTime >= timeLimit) {
-        clearInterval(timer);
-        alert("Time up!");
-      }
-    }, 1000);
-  }
-
-  // Helper function to get random index
-  function getRandomIndex() {
-    return Math.floor(Math.random() * (numpairs * 2));
-  }
-
-  // Fetch Pokémon data
-  const response = await axios.get(
-    "https://pokeapi.co/api/v2/pokemon?limit=810"
-  );
-  const data = response.data;
-  let randomPokemonUrls = [];
-  for (let k = 0; k < numpairs; k++) {
-    const randomPokemon =
-      data.results[Math.floor(Math.random() * data.results.length)];
-    const randomPokemonResponse = await axios.get(randomPokemon.url);
-    const randomPokemonData = randomPokemonResponse.data;
-    const randomPokemonImageUrl =
-      randomPokemonData.sprites.other["official-artwork"].front_default;
-    randomPokemonUrls.push(randomPokemonImageUrl);
-  }
-
-  // Create card elements dynamically
-  for (let i = 0; i < numpairs * 2; i++) {
-    let card = $("<div>").addClass("card");
-    let frontFace = $("<img>").addClass("front_face");
-    let backFace = $("<img>").addClass("back_face").attr("src", "back.webp");
-    card.append(frontFace, backFace);
-    gameGrid.append(card);
-  }
-
-  const usedIndices = [];
-  for (let j = 0; j < numpairs; j++) {
-    let randomIndex1 = getRandomIndex();
-    while (usedIndices.includes(randomIndex1)) {
-      randomIndex1 = getRandomIndex();
-    }
-    usedIndices.push(randomIndex1);
-
-    let randomIndex2 = getRandomIndex();
-    while (
-      usedIndices.includes(randomIndex2) ||
-      randomIndex2 === randomIndex1
-    ) {
-      randomIndex2 = getRandomIndex();
-    }
-    usedIndices.push(randomIndex2);
-
-    // Add Pokémon images to cards
-    gameGrid.children()[randomIndex1].children[0].src = randomPokemonUrls[j];
-    gameGrid.children()[randomIndex2].children[0].src = randomPokemonUrls[j];
-  }
-
-  // Add event listener to cards
-  $(".card").on("click", function () {
-    if (
-      $(this).hasClass("flip") ||
-      $(this).hasClass("matched") ||
-      openCards >= 2
-    ) {
-      return;
-    }
-
-    $(this).toggleClass("flip");
-    openCards++;
-    clickCount++;
-    $("#clickCount").text(clickCount);
-
-    if (!firstCard) firstCard = $(this);
-    else {
-      secondCard = $(this);
-
-      if (
-        firstCard.find(".front_face")[0].src ==
-        secondCard.find(".front_face")[0].src
-      ) {
-        // Match
-        firstCard.addClass("matched");
-        secondCard.addClass("matched");
-        matchedCards++;
-        $("#pairsMatched").text(matchedCards);
-        $("#pairsLeft").text(numpairs - matchedCards);
-        openCards = 0;
-        firstCard = secondCard = undefined;
-
-        setTimeout(checkWin, 1000);
-      } else {
-        // No match
-        setTimeout(() => {
-          firstCard.toggleClass("flip");
-          secondCard.toggleClass("flip");
-          openCards = 0;
-          firstCard = secondCard = undefined;
-        }, 1000);
-      }
-    }
+  // Difficulty selection
+  $("#easy-btn").on("click", function () {
+    difficulty = { pairs: 3, duration: 100 };
   });
 
-  $("#totalPairs").text(numpairs);
-  $("#pairsLeft").text(numpairs);
+  $("#medium-btn").on("click", function () {
+    difficulty = { pairs: 6, duration: 200 };
+  });
 
-  // Start the timer when the game starts
-  startTimer();
+  $("#hard-btn").on("click", function () {
+    difficulty = { pairs: 12, duration: 300 };
+  });
+
+  // Reset function
+  $("#reset-btn").on("click", function () {
+    $("#game-grid").empty();
+    $("#header").hide();
+    $("#difficulty-btns").show();
+    $("#action-btns").show();
+    $("#start-btn").show();
+    clearInterval(timer);
+    elapsedTime = 0;
+  });
+
+  // Start game function
+  $("#start-btn").on("click", function () {
+    startGame(difficulty.pairs, difficulty.duration);
+  });
+
+  // Hide header initially
+  $("#header").hide();
+
+  const startGame = async (numpairs, timeLimit) => {
+    $("#header").show();
+    $("#start-btn").hide();
+
+    // Check if all pairs are matched
+    function checkWin() {
+      if (matchedCards === numpairs) {
+        alert("You win!");
+        clearInterval(timer);
+      }
+    }
+
+    // Start timer
+    function startTimer() {
+      $("#timer").text(`You got ${timeLimit} seconds. 0 seconds passed.`);
+      timer = setInterval(() => {
+        elapsedTime++;
+        $("#timer").text(
+          `You got ${timeLimit} seconds. ${elapsedTime} seconds passed.`
+        );
+        if (elapsedTime >= timeLimit) {
+          clearInterval(timer);
+          alert("Time up!");
+        }
+      }, 1000);
+    }
+
+    // Helper function to get random index
+    function getRandomIndex() {
+      return Math.floor(Math.random() * (numpairs * 2));
+    }
+
+    // Fetch Pokémon data
+    const response = await axios.get(
+      "https://pokeapi.co/api/v2/pokemon?limit=810"
+    );
+    const data = response.data;
+    let randomPokemonUrls = [];
+    for (let k = 0; k < numpairs; k++) {
+      const randomPokemon =
+        data.results[Math.floor(Math.random() * data.results.length)];
+      const randomPokemonResponse = await axios.get(randomPokemon.url);
+      const randomPokemonData = randomPokemonResponse.data;
+      const randomPokemonImageUrl =
+        randomPokemonData.sprites.other["official-artwork"].front_default;
+      randomPokemonUrls.push(randomPokemonImageUrl);
+    }
+
+    // Create card elements dynamically
+    for (let i = 0; i < numpairs * 2; i++) {
+      let card = $("<div>").addClass("card");
+      let frontFace = $("<img>").addClass("front_face");
+      let backFace = $("<img>").addClass("back_face").attr("src", "back.webp");
+      card.append(frontFace, backFace);
+      gameGrid.append(card);
+    }
+
+    const usedIndices = [];
+    for (let j = 0; j < numpairs; j++) {
+      let randomIndex1 = getRandomIndex();
+      while (usedIndices.includes(randomIndex1)) {
+        randomIndex1 = getRandomIndex();
+      }
+      usedIndices.push(randomIndex1);
+
+      let randomIndex2 = getRandomIndex();
+      while (
+        usedIndices.includes(randomIndex2) ||
+        randomIndex2 === randomIndex1
+      ) {
+        randomIndex2 = getRandomIndex();
+      }
+      usedIndices.push(randomIndex2);
+
+      // Add Pokémon images to cards
+      gameGrid.children()[randomIndex1].children[0].src = randomPokemonUrls[j];
+      gameGrid.children()[randomIndex2].children[0].src = randomPokemonUrls[j];
+    }
+
+    // Add event listener to cards
+    $(".card").on("click", function () {
+      if (
+        $(this).hasClass("flip") ||
+        $(this).hasClass("matched") ||
+        openCards >= 2
+      ) {
+        return;
+      }
+
+      $(this).toggleClass("flip");
+      openCards++;
+      clickCount++;
+      $("#clickCount").text(clickCount);
+
+      if (!firstCard) firstCard = $(this);
+      else {
+        secondCard = $(this);
+
+        if (
+          firstCard.find(".front_face")[0].src ==
+          secondCard.find(".front_face")[0].src
+        ) {
+          // Match
+          firstCard.addClass("matched");
+          secondCard.addClass("matched");
+          matchedCards++;
+          $("#pairsMatched").text(matchedCards);
+          $("#pairsLeft").text(numpairs - matchedCards);
+          openCards = 0;
+          firstCard = secondCard = undefined;
+
+          setTimeout(checkWin, 1000);
+        } else {
+          // No match
+          setTimeout(() => {
+            firstCard.toggleClass("flip");
+            secondCard.toggleClass("flip");
+            openCards = 0;
+            firstCard = secondCard = undefined;
+          }, 1000);
+        }
+      }
+    });
+
+    $("#totalPairs").text(numpairs);
+    $("#pairsLeft").text(numpairs);
+
+    // Start the timer when the game starts
+    startTimer();
+  };
 };
 
 $(document).ready(setup);
